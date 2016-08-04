d705db552c016b4d9765dd8abed2744a480aa658
Salvador Rubio <Salvador Rubio>
2016-08-04 16:53:08 +0200

    movimiento jugadores

--- index.js
+++ index.js
@@ -26,27 +26,36 @@ http.listen(3000, function () {
 io.on('connection', function (socket) {
     console.log('a user connected ' + users);
     socket.emit('numberOfUsers', users);
-    
-    if (users > 0){
+
+    if (users > 0) {
         socket.emit('AllBubbles', bubbles);
     }
 
-    socket.on('sendBubbles', function (data){
+    socket.on('sendBubbles', function (data) {
+        var n = Date.now();
+        data.forEach(function (element) {
+            element.id =  n++;
+        }, this);
         bubbles = data;
         console.log(bubbles);
+        socket.emit('AllBubbles', bubbles);
     })
 
     users++;
     socket.on('disconnect', function () {
         console.log('user disconnected');
-        users = users - 1 ;
+        users = users - 1;
     });
 
-    socket.on('newPlayer', function(data){
+    socket.on('newPlayer', function (data) {    
         bubbles.push(data);
+        console.log('new player' + data);
         socket.broadcast.emit('newBubble', data);
     });
 
+    socket.on('updateBubble', function(data){
+         socket.broadcast.emit('getUpdateBubble', data);
+    })
     socket.on('new-message', function (params) {
         socket.broadcast.emit('messages', params.content);
     });
--- main.js
+++ main.js
@@ -9,28 +9,86 @@ var bubblesController = bubbles.controller('bubblesController', ['$scope', funct
     $scope.innerHeight = window.innerHeight - 20;
     $scope.bubbles;
     $scope.player;
+    var endGame;
 
     socket.on('numberOfUsers', function (data) {
         console.log("users " + data);
         if (data < 1) {
-            $scope.bubbles = createBubbles();
-            socket.emit('sendBubbles', $scope.bubbles);
+            //            $scope.bubbles = createBubbles();
+            socket.emit('sendBubbles', createBubbles());
         }
-        $scope.player = createPlayer();
+        $scope.player = createPlayer(data);
         socket.emit('newPlayer', $scope.player);
         $scope.$digest();
+        updatePlayer();
     })
 
     socket.on('AllBubbles', data => {
-        $scope.bubbles = data; 
-        $scope.$digest(); 
-        //console.log("AllBubbles: " + data);
+        $scope.bubbles = data;
+        $scope.$digest();
+        console.log("AllBubbles: " + JSON.stringify($scope.bubbles));
     });
 
-    socket.on('newBubble', function (data){
+    socket.on('newBubble', function (data) {
         $scope.bubbles.push(data);
+        console.log("new bubble : " + JSON.stringify(data));
         $scope.$digest();
     })
+
+    socket.on('getUpdateBubble', function (data) {
+        var index = $scope.bubbles.map((element, index) => {
+            if (element.id === data.id) {
+                return index;
+            } else return null;
+        }).find(a => a != null);
+        $scope.bubbles[index] = data;
+        $scope.$digest();
+    })
+
+    function updatePlayer() {
+        var speed;
+        var player = $scope.player;
+        if (player.size > 150) {
+            speed = 1;
+        } else if (player.size > 75) {
+            speed = 2;
+        } else {
+            speed = 3;
+        }
+
+        if (player.left) {
+            player.posX = player.posX + speed;
+        } else {
+            player.posX = player.posX - speed;
+        }
+        if (player.top) {
+            player.posY = player.posY + speed;
+        } else {
+            player.posY = player.posY - speed;
+        }
+
+        //limites de la pantalla para que rebote
+        if (player.posX > (window.innerWidth - (player.size / 2))) {
+            player.left = false;
+        }
+        if (player.posX < 0 + (player.size / 2)) {
+            player.left = true;
+        }
+        if (player.posY > (window.innerHeight - (player.size / 2))) {
+            player.top = false;
+        }
+        if (player.posY < 0 + (player.size / 2)) {
+            player.top = true;
+            ;
+        }
+
+        socket.emit('updateBubble', player);
+        $scope.player = player;
+        $scope.$digest();
+        endGame = setTimeout(updatePlayer, 50);
+    }
+
+
 
     function Bubble(size, type) {
         this.id;
@@ -56,10 +114,11 @@ var bubblesController = bubbles.controller('bubblesController', ['$scope', funct
         return bubbles;
     };
 
-    var createPlayer = _ =>{
+    var createPlayer = id => {
         var player = new Bubble(10, "blue");
         var playerName = window.prompt("Nombre de jugaor?");
         player.playerName = playerName != null ? playerName : "Desconocido";
+        player.id = id;
         return player;
     };
 
